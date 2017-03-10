65d7c0dbfaab711c8660a4f152d41b095d10b3ad
davidhenley <henleydr@gmail.com>
2017-03-10 14:39:13 -0600

    Handlebars helpers

--- server.js
+++ server.js
@@ -7,18 +7,30 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home',
-    welcomeMessage: 'Welcome!',
-    year: new Date().getFullYear()
+    welcomeMessage: 'Welcome to my website!'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About',
-    year: new Date().getFullYear()
+    pageTitle: 'About'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
   });
 });
 
