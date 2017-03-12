e06b358ffc6ebd5c820ea399851ce8693915412d
davidhenley <henleydr@gmail.com>
2017-03-12 11:40:24 -0500

    Maintenance middleware

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) console.log('Unable to append to log');
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
